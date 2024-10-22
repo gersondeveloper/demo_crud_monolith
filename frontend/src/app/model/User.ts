6317b32866eedf4d6850f4d7cfc0c06783979ecc
Gerson Filho <gerson.filho@kuehne-nagel.com>
2024-10-22 17:32:35 +0100

    feat: created data service and User model

---
+++ frontend/src/app/model/User.ts
@@ -0,0 +1,18 @@
+interface User {
+  id: number;
+  about: string;
+  address: string;
+  age: number;
+  balance: string;
+  company: string;
+  email: string;
+  eyeColor: string;
+  gender: string;
+  isActive: boolean;
+  latitude: number;
+  longitude: number;
+  name: string;
+  phone: string;
+  picture: string;
+  registered: string;
+}
---
+++ frontend/src/app/services/data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError, Observable, retry, throwError} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private apiUrl = "http://localhost:8080/api/";
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      )
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${error.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+
+}
